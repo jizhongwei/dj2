fdac0dcb1c2f2fd0716b53f0f1b7b572449b1c9d
jizhonwgei <jzwxiaoniao@163.com>
2019-10-05 20:55:03 +0800

    完善网站列表页和博客详情页布局

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 
@@ -17,6 +18,7 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blogs_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, id = blogs_type_pk)
+    context['blog_types'] = BlogType.objects.all()
     context['blogs'] = Blog.objects.filter(blog_type= blog_type)
     context['blog_type'] = blog_type
     return render_to_response('blog/blogs_with_type.html', context)
