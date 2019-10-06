30f9ce4ddb447106b7dcace6fa99d06b1f5c89ce
jizhonwgei <jzwxiaoniao@163.com>
2019-10-06 13:01:28 +0800

    博客模型增加阅读字段，并用cookie排除频繁刷新增加

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,8 @@ class Blog(models.Model):
     created_time = models.DateTimeField(auto_now_add= True)
     last_updated_time = models.DateTimeField(auto_now= True)
 
+    readed_num = models.IntegerField(default= 0)
+
     def __str__(self):
         return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -36,11 +36,15 @@ def blog_list(request):
 def blog_detail(request, blog_pk):
     context = {}
     blog = get_object_or_404(Blog, pk = blog_pk)
+    if not request.COOKIES.get('blog_%s_readed' % blog_pk):
+        blog.readed_num += 1
+        blog.save(update_fields= ['readed_num',])
     context['previous_blog'] = Blog.objects.filter(created_time__gt= blog.created_time).last()
     context['next_blog'] = Blog.objects.filter(created_time__lt= blog.created_time).first()
     context['blog'] = blog
-    print(context)
-    return render_to_response('blog/blog_detail.html', context)
+    response =  render_to_response('blog/blog_detail.html', context)
+    response.set_cookie('blog_%s_readed' % blog_pk, 'true', max_age= 60)
+    return response
 
 def blogs_with_type(request, blogs_type_pk):
     context = {}
