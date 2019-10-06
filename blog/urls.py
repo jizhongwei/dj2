a4efdaddb46247c4ba20aa3937e0d7149a4852cf
jizhonwgei <jzwxiaoniao@163.com>
2019-10-06 09:48:15 +0800

    日期归档

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:blog_pk>/', views.blog_detail, name = 'blog_detail'),
     path('', views.blog_list, name = 'blog_list'),
     path('type/<int:blogs_type_pk>/', views.blogs_with_type, name = 'blogs_with_type'),
+    path('date/<int:year>/<int:month>/', views.blogs_with_date, name = 'blogs_with_date'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -28,12 +28,17 @@ def blog_list(request):
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     context['page_range'] = page_range
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order= 'DESC')
     return render_to_response('blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_pk):
     context = {}
-    context['blog'] = get_object_or_404(Blog, pk = blog_pk)
+    blog = get_object_or_404(Blog, pk = blog_pk)
+    context['previous_blog'] = Blog.objects.filter(created_time__gt= blog.created_time).last()
+    context['next_blog'] = Blog.objects.filter(created_time__lt= blog.created_time).first()
+    context['blog'] = blog
+    print(context)
     return render_to_response('blog/blog_detail.html', context)
 
 def blogs_with_type(request, blogs_type_pk):
@@ -60,4 +65,36 @@ def blogs_with_type(request, blogs_type_pk):
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     context['page_range'] = page_range
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order= 'DESC')
+
     return render_to_response('blog/blogs_with_type.html', context)
+
+
+def blogs_with_date(request, year, month):
+    context = {}
+
+    blogs_all_list = Blog.objects.filter(created_time__year= year,
+                                         created_time__month= month)
+    paginator = Paginator(blogs_all_list, settings.NUMBER_BLOGS_PER_PAGE)
+    page_num = request.GET.get('page', 1)
+    page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + \
+                 list(range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
+
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context['blogs_with_date'] = '{}年{}月'.format(year, month)
+    context['page_of_blogs'] = page_of_blogs
+    context['blog_types'] = BlogType.objects.all()
+    context['page_range'] = page_range
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order= 'DESC')
+
+    return render_to_response('blog/blogs_with_date.html', context)
