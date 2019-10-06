4c8b0290c47d706789aaa65dc554384b33d9fda6
jizhonwgei <jzwxiaoniao@163.com>
2019-10-06 09:01:10 +0800

    完善博客分页

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.core.paginator import Paginator
 
+from django.conf import settings
+
 from .models import Blog, BlogType
 
 
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
-    paginator = Paginator(blogs_all_list, 10)
+    paginator = Paginator(blogs_all_list, settings.NUMBER_BLOGS_PER_PAGE)
     page_num = request.GET.get('page', 1)
     page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + \
+                 list(range(current_page_num, min(current_page_num + 2, paginator.num_pages)+1))
+
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
     context = {}
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
+    context['page_range'] = page_range
     return render_to_response('blog/blog_list.html', context)
 
 
@@ -23,7 +39,25 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blogs_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, id = blogs_type_pk)
-    context['blog_types'] = BlogType.objects.all()
-    context['blogs'] = Blog.objects.filter(blog_type= blog_type)
+    blogs_all_list = Blog.objects.filter(blog_type= blog_type)
+    paginator = Paginator(blogs_all_list, settings.NUMBER_BLOGS_PER_PAGE)
+    page_num = request.GET.get('page', 1)
+    page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + \
+                 list(range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
+
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
     context['blog_type'] = blog_type
+    context['page_of_blogs'] = page_of_blogs
+    context['blog_types'] = BlogType.objects.all()
+    context['page_range'] = page_range
     return render_to_response('blog/blogs_with_type.html', context)
